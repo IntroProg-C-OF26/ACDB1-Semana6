374875bef9f4eb8bf158c85364b6a6e6583850a9
Pedro Daniel Irene Robalino <pdirene@utpl.edu.ec>
2025-11-13 10:56:56 -0500

    Ejercicio 2

--- Proyecto-ACDB1-If/src/ExParcial_Votaciones.java
+++ Proyecto-ACDB1-If/src/ExParcial_Votaciones.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
 public class ExParcial_Votaciones {
-    
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+        String nombRecinto, nombJunta, resultadosElect;
+        int numEmpadronados, votosSi, votosNo, votosBlanco, votosNulo, votosEmitidos;
+        double porcParcipacion;
+        System.out.print("Dame Recinto, Junta, Emp, #Si, #No, #Blancos y #Nulos: ");
+        nombRecinto = tcl.next();   nombJunta = tcl.next();
+        numEmpadronados = tcl.nextInt(); votosSi = tcl.nextInt(); 
+        votosNo = tcl.nextInt(); votosBlanco = tcl.nextInt();
+        votosNulo = tcl.nextInt();
+        votosEmitidos = votosSi + votosNo + votosBlanco + votosNulo;
+        porcParcipacion = ( (double) votosEmitidos / (double) numEmpadronados) * 100;
+        if (votosSi > votosNo)
+            resultadosElect = "GANA SI";
+        else
+            resultadosElect = "GANA NO";
+        if (votosSi == votosNo)
+            resultadosElect = "EMPATE";
+        if (votosEmitidos > numEmpadronados)
+            resultadosElect = "FRAUDE";
+    }
 }
